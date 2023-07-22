c940e1045663df3862e4f819efe1ec5707797992
Athfan Khaleel <athphane@gmail.com>
2023-07-22 12:50:28 +0500

    wip: style the stats output with a table

--- overseerrbot/plugins/stats.py
+++ overseerrbot/plugins/stats.py
@@ -2,6 +2,7 @@ import json
 import sys
 from pprint import pprint
 
+from prettytable import PrettyTable
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -10,12 +11,16 @@ from overseerrbot.api.OverseerrApi import OverseerrApi
 
 
 @OverseerrBot.on_message(filters.command('stats'))
+@OverseerrBot.admins_only
 async def stats(_, message: Message):
     api = OverseerrApi()
     status = await api.get_requests_count()
 
-    response = []
-    for x in status:
-        response.append(f"{x.title()}: {status[x]}")
+    table = PrettyTable()
+    table.field_names = ["Status", "Count"]
+    table.align = "l"
 
-    await message.reply("\n".join(response))
+    for x in status:
+        table.add_row([x.title(), status[x]])
+
+    await message.reply(f"```{table}```")
